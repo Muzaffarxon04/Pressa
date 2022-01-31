dd21ec4ee79717bf1e3a4b283f0d0f3f84f1c4a1
Muzaffarxon04 <muzaffarxonabdusalomov04@gmail.com>
2022-01-31 23:20:37 +0500

    scss added

--- js/main.js
+++ js/main.js
@@ -1,31 +1,7 @@
-
-
-document.querySelector('.filter__form').addEventListener('submit', evt => {
-  evt.preventDefault()
-})
-
 const elBtns = document.querySelectorAll('.tip__btn')
-
-let Eldata = document.getElementById('date')
 let Elurl = document.querySelector('.filter__input')
-let Ellabel = document.querySelector('.label-time')
-let searchInput = document.querySelector('.nav__input')
-let magnifer = document.querySelector('.search-icon')
-
-Eldata.addEventListener('change', (e) => {
-  if (e.target.value) {
-    Ellabel.classList.add('label-time-active')
-  }
-})
 
 
-searchInput.addEventListener('keyup', (e) => {
-  if (e.target.value) {
-    magnifer.style.display = "none";
-  }else{
-    magnifer.style.display = "block";
-  }
-})
 
 Elurl.addEventListener('keyup', (e) => {
   if (!e.target.value) {
