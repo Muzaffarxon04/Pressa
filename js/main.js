0e1e543c58a9b3a8e1de56abcdca4222e58c46b1
Muzaffarxon04 <muzaffarxonabdusalomov04@gmail.com>
2022-02-01 15:31:45 +0500

    changes

--- js/main.js
+++ js/main.js
@@ -2,21 +2,23 @@ const elBtns = document.querySelectorAll('.tip__btn')
 let Elurl = document.querySelector('.filter__input')
 
 
-
 Elurl.addEventListener('keyup', (e) => {
   if (!e.target.value) {
     Elurl.style.backgroundImage = "url('../img/location.svg')";
   } else {
-    Elurl.style.backgroundImage = "url('./location.svg')";
+    Elurl.style.backgroundImage = "url('')";
   }
 })
+
 
 elBtns.forEach(btn => {
   btn.addEventListener('click', evt => {
     elBtns[0].classList.remove('tip__btn--active')
     elBtns[1].classList.remove('tip__btn--active')
-
+    
     btn.classList.toggle('tip__btn--active')
+    
+    console.log(evt.target);
   })
 })
 
